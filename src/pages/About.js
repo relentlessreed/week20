33443ccd777dc0124b8d4241471835e4b187c04e
Edward Reed <relentlessreed@gmail.com>
2021-08-10 21:20:33 -0500

    FINAL Text tweaks

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,8 +6,8 @@ export default function About() {
     <div className="aboutMeContainer">
       <img src={headshotSmallImg} alt="HeadShot" />
       <div id="aboutCard">
-        I am a 2021 Kansas University full-stack software developer graduate, with experience in front and backend. I have a passion for building applications to help better
-        the world, and a strong drive to always be learning about tech! When I am not coding, I am either cooking/skateboarding/playing music/ or travelling.
+        <p id="aboutText">I am a 2021 Kansas University full-stack software developer graduate, with experience in front and backend. I have a passion for building applications to help better
+          the world, and a strong drive to always be learning about tech! When I am not coding, I am either cooking/skateboarding/playing music/ or travelling.</p>
       </div>
     </div>
   );
