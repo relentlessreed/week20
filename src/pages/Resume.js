adcc4c000304834f23f960f6a7ab7608c8ae406b
Edward Reed <relentlessreed@gmail.com>
2021-08-10 20:34:43 -0500

    MVP

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,20 +2,10 @@ import React from 'react';
 
 export default function Resume() {
   return (
-    <div>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div id="resumeLink">
+      <h2>List of Proficencies:</h2>
+      <p>HTML5, CSS, JQuery, JavaScript, React, Handlebars.js, Node.js, Mongo.db, MySql, Sequelize, Express.js, JawsDB, Bcrypt, Dotenv, Bootstrap, Figma, Tailwind.css, Git, Github, Gitlab, Heroku, JSON, API's, Cookies, FontAwesome, DayJS, NoSql, RegEx, IndexedDB, Chrome Lighthouse Optimization, PWA's, MongoDB, Mongoose, MongoAtlas, MERN Stack, MVC, GraphyQL, Apollo, JSON Web Tokens.</p>
+      <a href="https://drive.google.com/file/d/1rkI5SJqf7dZ4kHVvZjpA8B05CBgIEd06/view?usp=sharing" target="_blank" rel="noreferrer">Downloadable Copy of my Resume</a>
     </div>
   );
 }
