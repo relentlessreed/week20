43f9f6e6bb34f2dc8483b8886e9ff5c12a424d37
Edward Reed <relentlessreed@gmail.com>
2021-08-10 20:43:43 -0500

    Final tweaks on resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default function Resume() {
   return (
-    <div id="resumeLink">
-      <h2>List of Proficencies:</h2>
-      <p>HTML5, CSS, JQuery, JavaScript, React, Handlebars.js, Node.js, Mongo.db, MySql, Sequelize, Express.js, JawsDB, Bcrypt, Dotenv, Bootstrap, Figma, Tailwind.css, Git, Github, Gitlab, Heroku, JSON, API's, Cookies, FontAwesome, DayJS, NoSql, RegEx, IndexedDB, Chrome Lighthouse Optimization, PWA's, MongoDB, Mongoose, MongoAtlas, MERN Stack, MVC, GraphyQL, Apollo, JSON Web Tokens.</p>
-      <a href="https://drive.google.com/file/d/1rkI5SJqf7dZ4kHVvZjpA8B05CBgIEd06/view?usp=sharing" target="_blank" rel="noreferrer">Downloadable Copy of my Resume</a>
+    <div class="resumePageBody">
+      <div id="resumePageText">
+        <h2>List of Proficencies:</h2>
+        <p>HTML5, CSS, JQuery, JavaScript, React, Handlebars.js, Node.js, Mongo.db, MySql, Sequelize, Express.js, JawsDB, Bcrypt, Dotenv, Bootstrap, Figma, Tailwind.css, Git, Github, Gitlab, Heroku, JSON, API's, Cookies, FontAwesome, DayJS, NoSql, RegEx, IndexedDB, Chrome Lighthouse Optimization, PWA's, MongoDB, Mongoose, MongoAtlas, MERN Stack, MVC, GraphyQL, Apollo, JSON Web Tokens.</p>
+        <a href="https://drive.google.com/file/d/1rkI5SJqf7dZ4kHVvZjpA8B05CBgIEd06/view?usp=sharing" target="_blank" rel="noreferrer">Downloadable Copy of my Resume</a>
+      </div>
     </div>
   );
 }
